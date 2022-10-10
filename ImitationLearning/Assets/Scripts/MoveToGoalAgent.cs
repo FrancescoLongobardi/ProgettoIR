80d8a93b4686e59770b1d3540c1a1612536769b7
azankiew <checcol98@hotmail.it>
2022-10-10 20:33:24 +0200

    primo addestramento

--- ImitationLearning/Assets/Scripts/MoveToGoalAgent.cs
+++ ImitationLearning/Assets/Scripts/MoveToGoalAgent.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using UnityEngine;
 
 public class MoveToGoalAgent : Agent
 {   
-    private float speed = 3f;
+    private float speed = 5f;
+    public GameObject door;
+
+    public override void OnEpisodeBegin(){
+        float pos_x_pl = UnityEngine.Random.Range(-9.0f,9.0f);
+        float pos_z_pl = UnityEngine.Random.Range(-9.0f,9.0f);
+        transform.position = new Vector3(pos_x_pl,0.1f,pos_z_pl);
+        transform.rotation = Quaternion.identity;
+        float pos_x_door, pos_z_door, distance;
+        do{
+            pos_x_door = UnityEngine.Random.Range(-8.0f,8.0f);
+            pos_z_door = UnityEngine.Random.Range(-8.0f,8.0f);
+            distance = Vector3.Distance(transform.position, new Vector3(pos_x_door,0.1f,pos_z_door));
+        }while(distance < 0.3f);
+        
+        door.transform.position = new Vector3(pos_x_door,0.1f,pos_z_door);
+
+    }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        if(transform.position.y < 0){
+            AddReward(-1.0f);
+            Debug.Log("-1");
+            EndEpisode();
+        }
         float move_x = actions.ContinuousActions[0];
         float move_z = actions.ContinuousActions[1];
 
         transform.position += new Vector3(move_x, 0, move_z) * Time.deltaTime * speed;
 
+        float distance = Vector3.Distance(transform.position, door.transform.position);
+        AddReward((0.001f/distance)-0.0002f);
+        Debug.Log(GetCumulativeReward());
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -27,7 +53,16 @@ public class MoveToGoalAgent : Agent
     }
 
     public void OnTriggerEnter(Collider other){
-        Debug.Log("Tocco");
+        if (other.TryGetComponent<Goal>(out Goal goal)){
+            AddReward(1.0f);
+            Debug.Log("1");
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall)){
+            AddReward(-1.0f);
+            Debug.Log("-1");
+            EndEpisode();
+        }
     }
     
 }
